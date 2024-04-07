178c1f65fc4bfd737e4c4530cee17f773a9b44b4
Muhammad-Aasad <m96999276@gmail.com>
2024-04-07 22:48:27 +0500

    commit

--- main.js
+++ main.js
@@ -1,32 +1,32 @@
 import inquirer from "inquirer";
 const currency = {
-    USD: 1,
+    USD: 1, //Base currency
     EUR: 0.91,
     GPB: 0.76,
     INR: 74.54,
-    PKR: 280
+    PKR: 280,
 };
 let user_Answer = await inquirer.prompt([
     {
         name: "from",
         message: "Enter From Currency",
         type: "list",
-        choices: ["USD", "EUR", "GPB", "INR", "PKR"]
+        choices: ["USD", "EUR", "GPB", "INR", "PKR"],
     },
     {
         name: "to",
         message: "Enter To Currency",
         type: "list",
-        choices: ["USD", "EUR", "GPB", "INR", "PKR"]
+        choices: ["USD", "EUR", "GPB", "INR", "PKR"],
     },
     {
         name: "amount",
         message: "Enter Your Amount",
-        type: 'number',
+        type: "number",
     },
 ]);
-let formamount = currency[user_Answer.from];
-let toamount = currency[user_Answer.to];
+let formamount = currency[user_Answer.from]; //exchange rate 
+let toamount = currency[user_Answer.to]; //exchange rate 
 let amount = user_Answer.amount;
 let baseamount = amount / formamount; //USD base currency // 4
 let covertedamount = baseamount * toamount;
--- main.ts
+++ main.ts
@@ -1,42 +1,40 @@
-import inquirer  from "inquirer"
+import inquirer from "inquirer";
 
-const currency : any = {
-    USD:1,
-    EUR:0.91,
-    GPB:0.76,
-    INR:74.54,
-    PKR:280
-}
+const currency: any = {
+  USD: 1, //Base currency
+  EUR: 0.91,
+  GPB: 0.76,
+  INR: 74.54,
+  PKR: 280,
+};
 
-let user_Answer = await inquirer.prompt(
-    [    
-        {
-            name:"from",
-            message:"Enter From Currency",
-            type:"list",
-            choices:["USD","EUR","GPB","INR","PKR"]
-        },
+let user_Answer = await inquirer.prompt([
+  {
+    name: "from",
+    message: "Enter From Currency",
+    type: "list",
+    choices: ["USD", "EUR", "GPB", "INR", "PKR"],
+  },
 
-        {
-            name:"to",
-            message:"Enter To Currency",
-            type:"list",
-            choices:["USD","EUR","GPB","INR","PKR"]
-        },
+  {
+    name: "to",
+    message: "Enter To Currency",
+    type: "list",
+    choices: ["USD", "EUR", "GPB", "INR", "PKR"],
+  },
 
-        {
-            name:"amount",
-            message:"Enter Your Amount",
-            type:'number',
-        },
-    ]
-)
+  {
+    name: "amount",
+    message: "Enter Your Amount",
+    type: "number",
+  },
+]);
 
-let formamount = currency[user_Answer.from]
-let toamount = currency[user_Answer.to]
-let amount = user_Answer.amount
-let baseamount = amount / formamount  //USD base currency // 4
-let covertedamount = baseamount * toamount
+let formamount = currency[user_Answer.from];    //exchange rate 
+let toamount = currency[user_Answer.to];   //exchange rate 
+let amount = user_Answer.amount;
+let baseamount = amount / formamount; //USD base currency // 4
+let covertedamount = baseamount * toamount;
 
 console.log(formamount);
 console.log(toamount);
